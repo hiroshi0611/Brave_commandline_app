43d04ba455036f95e63057c02345f71253d12143
imaizumi <metallicaslayer2002@yahoo.co.jp>
2019-11-27 23:46:59 +0900

    ゲッターとセッター

--- main.rb
+++ main.rb
@@ -1,18 +1,34 @@
 class Brave
     def name=(name)
-        @nemae = name
+        @name = name
+    end
+
+    def name 
+        @name
     end
 
     def hp=(hp)
         @hp = hp
     end
 
+    def hp
+        @hp
+    end
+
     def offense=(offense)
         @offense = offense
     end
 
+    def offense
+        @offense
+    end
+
     def defense=(defense)
         @defense = defense
+    end
+
+    def defense
+        @defense
     end
 end
 
@@ -22,3 +38,8 @@ brave.name = 'テリー'
 brave.hp = 500
 brave.offense = 150
 brave.defense = 100
+
+puts "NAME:#{brave.name}"
+puts "HP:#{brave.hp}"
+puts "OFFENSE:#{brave.offense}"
+puts "DEFENSE:#{brave.defense}"
