33fde6733ac640da63f2e90e7e07a4ee50cd9c74
imaizumi <metallicaslayer2002@yahoo.co.jp>
2019-11-28 14:07:53 +0900

    attackメソッド作成

--- main.rb
+++ main.rb
@@ -12,6 +12,18 @@ class Brave
         @offense = params[:offense]
         @defense = params[:defense]
     end
+
+    def attack(monster)
+        # monster.hp = monster.hp - damage
+        puts "#{@name}の攻撃!"
+        
+        damage = @offense - monster.defense
+
+        monster.hp -= damage
+
+        puts "#{monster.name}は#{damage}のダメージを受けた!"
+        puts "#{monster.name}の残りHPは#{monster.hp}だ"
+    end
 end
 
 class Monster
@@ -29,12 +41,7 @@ class Monster
 end
 
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
-
-puts "NAME:#{brave.name}"
-puts "HP:#{brave.hp}"
-puts "OFFENSE:#{brave.offense}"
-puts "DEFENSE:#{brave.defense}"
-
-brave.hp -= 30
-
 monster = Monster.new(name: 'スライム', hp: 250, offense: 200,defense: 100)
+
+
+brave.attack(monster)
